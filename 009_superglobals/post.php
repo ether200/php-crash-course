5af3cdf7121e05a80ba2479bf4d3da202da61658
Ivan <ggwpdiaf@gmail.com>
2021-07-05 15:29:54 -0300

    added superglobal folder

---
+++ 009_superglobals/post.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+// POST
+// Collect data from the HTML form (can not be seen from client)
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo $_POST['name'] . ", your form is submitted.";
+
+    echo"<br>";
+    print_r($_POST);
+}
+    
+?>
+
+<form action="post.php" method="post">
+    <input type="text" name="name">
+    <input type="text" name="age">
+    <button type="submit">SUBMIT</button>
+</form>
+    
+</body>
+</html>
